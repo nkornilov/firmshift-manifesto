97cf3b6bb839191b60ae5c508304fc4a00524379
Nikita Kornilov <n.b.kornilov@gmail.com>
2016-09-27 13:09:46 +0300

    update

--- public/js/app.js
+++ public/js/app.js
@@ -1,33 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-  var manifestos = [
-    {
-      text: "use astronomy to make your move in music industry"
-    },
-    {
-      text: "from online insuring to a sure thing"
-    },
-    {
-      text: "from big pharma to modern farming"
-    },
-    {
-      text: "to be on top in banking you need a super computer and a wizkid"
-    }
-  ];
-
-  astronomy_manifesto({
-    region: ".js-hc-container"
-  });
-
-  $("body").on('click', function () {
-    if (window.isAnimating) return false;
-
-    astronomy_manifesto({
-      region: ".js-hc-container"
-    });
-  })
-
-
-
+  animation();
 
 });
