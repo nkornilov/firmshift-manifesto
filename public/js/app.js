46044b08564cf184a72eef97060cb412239a7fe7
Nikita Kornilov <n.b.kornilov@gmail.com>
2016-09-28 14:50:19 +0100

    Revert "update"
    This reverts commit 97cf3b6bb839191b60ae5c508304fc4a00524379.

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-  animation();
+  var manifestos = [
+    {
+      text: "use astronomy to make your move in music industry"
+    },
+    {
+      text: "from online insuring to a sure thing"
+    },
+    {
+      text: "from big pharma to modern farming"
+    },
+    {
+      text: "to be on top in banking you need a super computer and a wizkid"
+    }
+  ];
+
+  astronomy_manifesto({
+    region: ".js-hc-container"
+  });
+
+  $("body").on('click', function () {
+    if (window.isAnimating) return false;
+
+    astronomy_manifesto({
+      region: ".js-hc-container"
+    });
+  })
+
+
+
 
 });
